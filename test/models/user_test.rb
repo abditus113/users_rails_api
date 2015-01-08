3ea7e8ba211dc3a945db54d7ba8065370eecf97b
Łukasz Wojtczak <abditus113@gmail.com>
2015-01-08 01:11:18 +0100

    test user validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  test "should validate when age absent" do
+    user = User.new(name: 'john')
+    assert user.valid?
+  end
+
+  test "should have integer numerical age if any" do
+    user = User.new(name: 'john', age: 2.5)
+    assert_not user.valid?
+    assert_equal ["must be an integer"], user.errors.messages[:age]
+  end
+
+  test "should have numerical age if any" do
+    user = User.new(name: 'john', age: 'aaa')
+    assert_not user.valid?
+    assert_equal ["is not a number"], user.errors.messages[:age]
+  end
+
+  test "should have positive, numerical age if any" do
+    user = User.new(name: 'john', age: 0)
+    assert_not user.valid?
+    assert_equal ["must be greater than 0"], user.errors.messages[:age]
+  end
+end

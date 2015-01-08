7a286f2fd1c77830f70a837fa57f8b85956bf2e4
Łukasz Wojtczak <abditus113@gmail.com>
2015-01-08 11:13:59 +0100

    little bit of cleanup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  setup do
-    @user = users(:one)
-  end
-
   test "should get index" do
     get :index
     assert_response :success
